33b4a21bfc4fb5b1e8f128b3bc3577c10ea88711
tomislav-ficko <ficko.tomislav@gmail.com>
2020-11-22 20:37:35 +0100

    Add set up Bottom Sheet behaviour

--- app/src/main/java/hr/ficko/firstar/MainActivity.kt
+++ app/src/main/java/hr/ficko/firstar/MainActivity.kt
@@ -2,10 +2,35 @@ package hr.ficko.firstar
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.TypedValue
+import android.view.View
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import kotlinx.android.synthetic.main.activity_main.*
+
+private const val BOTTOM_SHEET_PEEK_HEIGHT = 50f
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setupBottomSheet()
+    }
+
+    private fun setupBottomSheet() {
+        val bottomSheetBehavior = BottomSheetBehavior.from(bottomSheet)
+        bottomSheetBehavior.peekHeight = TypedValue.applyDimension(
+            TypedValue.COMPLEX_UNIT_DIP,
+            BOTTOM_SHEET_PEEK_HEIGHT,
+            resources.displayMetrics
+        ).toInt()
+        bottomSheetBehavior.addBottomSheetCallback(
+            object : BottomSheetBehavior.BottomSheetCallback() {
+                override fun onStateChanged(bottomSheet: View, newState: Int) {
+                    bottomSheet.bringToFront()
+                }
+
+                override fun onSlide(bottomSheet: View, slideOffset: Float) {}
+            })
     }
 }
